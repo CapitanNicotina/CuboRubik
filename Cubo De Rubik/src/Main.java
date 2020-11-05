d432a090bd6f16a724138f69a3e628d2d1e89bf9
ehast <ehast@host.docker.internal>
2020-11-05 13:15:52 -0500

    le gusta perrear

--- Cubo De Rubik/src/Main.java
+++ Cubo De Rubik/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Edwin es fanatico de lo sensual \nEl tiene una foto mia");
+		System.out.println("Edwin es fanatico de lo sensua, a el le gusta perrear");
 	}
 
 }
