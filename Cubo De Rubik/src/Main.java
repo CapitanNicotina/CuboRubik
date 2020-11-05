0c8bf47accb8d62327747ce059ac097dfafe3616
cypro <cypro@192.168.39.126>
2020-11-05 13:09:55 -0500

    Edwin es es un travieso

--- Cubo De Rubik/src/Main.java
+++ Cubo De Rubik/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Edwin es fanatico de lo sensual \nEl tiene una foto mia");
 	}
 
 }
